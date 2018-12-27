47cf8a726d58fbc910124d1e23773a80a7615d52
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-27 12:27:43 -0500

    added NavBar icons

--- src/components/NavBarMenu.js
+++ src/components/NavBarMenu.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Header, Segment} from 'semantic-ui-react'
+import {Header, Segment, Icon} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 const NavBarMenu = () => {
   return(
     <Segment clearing inverted>
-     <Header color='teal' as='h2' floated='right'>
+     <Header color='teal' as='h3' floated='right'>
+       <Icon name="user circle" size="mini"/>
        MemberName
      </Header>
      <Header color='teal' as='h2' floated='left'>
-       <Link to="/group/events">Group Name</Link>
+       <Link to="/group/events"><Icon name="home"/>Group Name</Link>
      </Header>
    </Segment>
   )
