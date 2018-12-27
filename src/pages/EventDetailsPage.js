96d21b64bb31a862d5137a7bcf1e13bf4202e999
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-27 12:13:07 -0500

    more styling on Events Details Page

--- src/pages/EventDetailsPage.js
+++ src/pages/EventDetailsPage.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import NavBarMenu from '../components/NavBarMenu'
 import MembersAttendingContainer from '../containers/MembersAttendingContainer'
-import {Header, Segment} from 'semantic-ui-react'
+import {Header, Segment, Form, Checkbox, Button, Grid} from 'semantic-ui-react'
 
 const EventDetailsPage = () => {
   return(
     <div>
       <NavBarMenu />
 
-      <Segment >
-        <Header as='h3'>Event Details</Header>
+      <Segment clearing className="details">
+        <Header as='h2' floated='right'>
+          <Form>
+            <Grid>
+              <Grid.Row relaxed container columns={4}>
+            <Form.Field>
+                <Checkbox label='Going ' />
+            </Form.Field>
+            <Form.Field>
+                <Checkbox label='Maybe ' />
+            </Form.Field>
+            <Form.Field>
+                <Checkbox label='Not Going ' />
+            </Form.Field>
+            <Button size="mini" compact type='submit'>Save</Button>
+              </Grid.Row>
+            </Grid>
+          </Form>
+        </Header>
+        <Header as='h2' floated='left'>
+          Event Details
+        </Header>
       </Segment>
 
-      <Segment>
+      <Segment className="details">
       Description
       </Segment>
 
-      <Segment >
+      <Segment className="details">
         Date
         Time
       </Segment>
-      <Segment >
+      <Segment className="details">
         Location
       </Segment>
 
